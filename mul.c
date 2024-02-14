28c109f60015d8b2ce397eec1470561ce98c0eaa
srujanachakka <chakkasruji556@gmail.com>
2024-02-14 11:38:30 +0530

    Multiplication program

---
+++ mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, product;
+
+    // Input the first number
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    // Input the second number
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    // Multiply the numbers
+    product = num1 * num2;
+
+    // Display the result
+    printf("The product of %d and %d is: %d\n", num1, num2, product);
+
+    return 0;
+}
+
